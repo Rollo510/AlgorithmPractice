c6d7d5fdd31981e1653d1ae3f81bb65c17c7d71e
kbritt510 <kbritt510@gmail.com>
2021-05-03 23:14:50 -0500

    coded solution to reverseInteger algorithm

---
+++ ReverseInteger.js
@@ -0,0 +1,5 @@
+const reverseInteger = (x) => {
+    if (x < 0) return -1 * reverseInteger(-x);
+    const solution = (x+"").split('').reverse().join('');
+    return (solution > 2**31 -1) ? 0 : solution;
+}
